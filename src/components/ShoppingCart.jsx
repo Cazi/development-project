6e02ea7e517a79c6fa3b66931421546a9990649b
Cazi <71891696+Cazi@users.noreply.github.com>
2023-01-14 09:43:06 -0500

    Working on quantity now

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -1,7 +1,18 @@
 import '../styles/CartStyle.css'
 import CartItem from './CartItem';
 const ShoppingCart = ({ cart }) => {
-    let total = 0
+    let total = 0;
+    function Counter(array) {
+        let count = {}
+        for (let index = 0; index < array.length; index++) {
+            if (array[index].name in count) {
+                count[array[index].name] += 1;
+            } else {
+                count[array[index].name] = 1;
+            }
+        }
+      }
+    let countItems = Counter(cart)
     return (
         <div className='cart'>
             <span className="cartHeading"> Cart</span>
@@ -10,6 +21,7 @@ const ShoppingCart = ({ cart }) => {
                     <CartItem item={item} key={key} />
                 )
             })}
+            {console.log({countItems})}
         </div>
     );
 }

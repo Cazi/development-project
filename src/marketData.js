bc303665477d4dd41462d4d82504f6a632502991
Cazi <71891696+Cazi@users.noreply.github.com>
2023-01-15 18:00:49 -0500

    Added new market items

--- src/marketData.js
+++ src/marketData.js
@@ -1,27 +1,87 @@
+// genre: "action",
+// genre: "mystery",
+// genre: "romance",
 export const marketData = [
     {
         name: "Soul Eater Vol. 1",
         imgUrl: './images/soul eater.jpg',
         price: 15.99,
         id: 1,
+        genre: "action",
     },
     {
         name: "Naruto Vol. 1",
         imgUrl: "images/naruto vol 1.jpg",
         price: 15.99,
         id: 2,
+        genre: "action",
     },
     {
         name: "Hunter x Hunter Vol. 1",
         imgUrl: "images/hxh vol1.jpg",
-        price: 15.99,
+        price: 10.99,
         id: 3,
+        genre: "action",
     },
     {
         name: "Chainsaw Man Vol. 1",
-        imgUrl: "images/soul eater.jpg",
-        price: 15.99,
+        imgUrl: "images/chainsaw.jpg",
+        price: 9.99,
         id: 4,
+        genre: "action",
     },
+    {
+        name: "Erased Vol. 1",
+        imgUrl: './images/erased.jpg',
+        price: 17.99,
+        id: 1,
+        genre: "mystery",
+    },
+    {
+        name: "Detective Conan Vol. 1",
+        imgUrl: "images/conan.jpg",
+        price: 12.99,
+        id: 2,
+        genre: "mystery",
+    },
+    {
+        name: "Durarara Vol. 1",
+        imgUrl: "images/durarara.jpg",
+        price: 10.99,
+        id: 3,
+        genre: "mystery",
+    },
+    {
+        name: "Serial Experiments Lain Vol. 1",
+        imgUrl: "images/lain.jpg",
+        price: 19.99,
+        id: 4,
+        genre: "mystery",
+    },
+    {
+        name: "Toradora Vol. 1",
+        imgUrl: "images/toradora.jpg",
+        price: 12.99,
+        id: 2,
+        genre: "romance",
+    },
+    {
+        name: "Clannad Vol. 1",
+        imgUrl: "images/clannad.jpg",
+        price: 15.99,
+        id: 3,
+        genre: "romance",
+    },
+    {
+        name: "The Demon Prince Of Momochi House Vol. 1",
+        imgUrl: "images/prince.jpg",
+        price: 21.99,
+        id: 4,
+        genre: "romance",
+    },
+
+
+
+
 ];
     

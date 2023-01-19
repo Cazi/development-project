5625bfb9186d53ed37e2ca8acb59e9900cebccb4
Cazi <71891696+Cazi@users.noreply.github.com>
2023-01-18 19:23:24 -0500

    Added filter buttons

--- src/components/Market.jsx
+++ src/components/Market.jsx
@@ -1,17 +1,22 @@
 import '../styles/MarketStyle.css'
 import MarketItem from './MarketItem'
 import { marketData } from '../marketData'
-const Market = ({addCart}) => {
+const Market = ({ addCart }) => {
   return (
     <div className="market">
       <span className="marketHeading">Market</span>
-        {marketData.map((data, key) => {
-          return (
-            <div key={key} className="marketItem">
-              <MarketItem name={data.name} imageUrl={data.imgUrl} price={data.price} addCart={addCart} item={data}/>
-            </div>
-          );
-        })}
+      <div className='market-filters'>
+        <button>Mystery</button>
+        <button>Action</button>
+        <button>Romance</button>
+      </div>
+      {marketData.map((data, key) => {
+        return (
+          <div key={key} className="marketItem">
+            <MarketItem name={data.name} imageUrl={data.imgUrl} price={data.price} addCart={addCart} item={data} />
+          </div>
+        );
+      })}
 
 
     </div>

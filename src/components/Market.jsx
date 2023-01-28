2b5f3f89a3093df42d215af63142a08436152ba7
Cazi <71891696+Cazi@users.noreply.github.com>
2023-01-28 15:09:08 -0500

    Filter buttons work

--- src/components/Market.jsx
+++ src/components/Market.jsx
@@ -1,16 +1,34 @@
 import '../styles/MarketStyle.css'
 import MarketItem from './MarketItem'
 import { marketData } from '../marketData'
+import { useState, useEffect } from 'react'
 const Market = ({ addCart }) => {
+  const [items, setItems] = useState(marketData);
+  // useEffect(() => {
+  //   setItems({
+  //     items: items
+  //   })
+  // });
+
+  const handleClick = (genre) => {
+    setItems(marketData)
+    let filterItems = []
+    if (genre !== 'all') {
+      filterItems = items.filter(item => item.genre === genre);
+      setItems(filterItems)
+    }
+  }
+
   return (
     <div className="market">
       <span className="marketHeading">Market</span>
       <div className='market-filters'>
-        <button>Mystery</button>
-        <button>Action</button>
-        <button>Romance</button>
+        <button onClick={() => handleClick('all')}>Reset</button>
+        <button onClick={() => handleClick('mystery')}>Mystery</button>
+        <button onClick={() => handleClick('action')}>Action</button>
+        <button onClick={() => handleClick('romance')}>Romance</button>
       </div>
-      {marketData.map((data, key) => {
+      {items.map((data, key) => {
         return (
           <div key={key} className="marketItem">
             <MarketItem name={data.name} imageUrl={data.imgUrl} price={data.price} addCart={addCart} item={data} />
@@ -22,5 +40,6 @@ const Market = ({ addCart }) => {
     </div>
   )
 }
+
 
 export default Market
